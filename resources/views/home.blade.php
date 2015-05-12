b672caf8ffe8b008d462b6aba582d4d22ae302d4
Benjamin Wright <benjamin@davenporttheatrical.com>
2015-05-12 15:41:01 -0400

    Setup basic review layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,91 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
+            <!-- LISTED ITEMS IN THESE PANELS -->
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
+                <!-- PUT YOUR CONTENT IN THE PANEL BODY -->
+				<div class="panel-body review-panel">
+                    <div class="review">
+					&ldquo; This is a review.&rdquo;
+                    </div>
+                    <div class="twitter-handle">
+                    - @twitterhandle
+                    </div>
 				</div>
+                <div class="up-down">
+                    <a href="#" class="show-up">Up</a>
+                    <div class="up-score">10</div>
+                    <a href="#" class="show-down">Down</a>
+                </div>
 			</div>
+            <!-- LISTED ITEMS IN THESE PANELS -->
+            <div class="panel panel-default">
+                <!-- PUT YOUR CONTENT IN THE PANEL BODY -->
+                <div class="panel-body review-panel">
+                    <div class="review">
+                    &ldquo; This is a review.&rdquo;
+                    </div>
+                    <div class="twitter-handle">
+                    - @twitterhandle
+                    </div>
+                </div>
+                <div class="up-down">
+                    <a href="#" class="show-up">Up</a>
+                    <div class="up-score">10</div>
+                    <a href="#" class="show-down">Down</a>
+                </div>
+            </div>
+             <!-- LISTED ITEMS IN THESE PANELS -->
+            <div class="panel panel-default">
+                <!-- PUT YOUR CONTENT IN THE PANEL BODY -->
+                <div class="panel-body review-panel">
+                    <div class="review">
+                    &ldquo; This is a review.&rdquo;
+                    </div>
+                    <div class="twitter-handle">
+                    - @twitterhandle
+                    </div>
+                </div>
+                <div class="up-down">
+                    <a href="#" class="show-up">Up</a>
+                    <div class="up-score">10</div>
+                    <a href="#" class="show-down">Down</a>
+                </div>
+            </div>
+             <!-- LISTED ITEMS IN THESE PANELS -->
+            <div class="panel panel-default">
+                <!-- PUT YOUR CONTENT IN THE PANEL BODY -->
+                <div class="panel-body review-panel">
+                    <div class="review">
+                    &ldquo; This is a review.&rdquo;
+                    </div>
+                    <div class="twitter-handle">
+                    - @twitterhandle
+                    </div>
+                </div>
+                <div class="up-down">
+                    <a href="#" class="show-up">Up</a>
+                    <div class="up-score">10</div>
+                    <a href="#" class="show-down">Down</a>
+                </div>
+            </div>
+             <!-- LISTED ITEMS IN THESE PANELS -->
+            <div class="panel panel-default">
+                <!-- PUT YOUR CONTENT IN THE PANEL BODY -->
+                <div class="panel-body review-panel">
+                    <div class="review">
+                    &ldquo; This is a review.&rdquo;
+                    </div>
+                    <div class="twitter-handle">
+                    - @twitterhandle
+                    </div>
+                </div>
+                <div class="up-down">
+                    <a href="#" class="show-up">Up</a>
+                    <div class="up-score">10</div>
+                    <a href="#" class="show-down">Down</a>
+                </div>
+            </div>
 		</div>
 	</div>
 </div>
