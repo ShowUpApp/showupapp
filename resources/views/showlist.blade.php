8a9d6c4f346acf6e3db0ced9f0984ad0ecf588a2
di_tang <1NANDAtianwen>
2015-05-12 11:48:29 -0400

    new view for showlist

---
+++ resources/views/showlist.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+<div class="container" style="background-color: red;">
+    <div class="content">
+        <div class="title">testing</div>
+
+    </div>
+</div>
+</body>
+</html>
